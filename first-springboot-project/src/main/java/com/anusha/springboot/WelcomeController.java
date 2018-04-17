0306cf8f21a62001602e3acf5688d30bdd90dd27
srinivas-godhala <srinivas.godhala@accenture.com>
2018-04-16 18:50:24 -0700

    First commit

---
+++ first-springboot-project/src/main/java/com/anusha/springboot/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.anusha.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class WelcomeController {
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return "hi how r u doing";
+	}
+
+}
